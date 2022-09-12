8d1579720cd9525f7db851677474069e5f212f46
Rene-Ngong <99443806+Rene-Ngong@users.noreply.github.com>
2022-09-12 17:25:46 -0500

    Testing new changes for s3 bucket

--- createS3bucket.py
+++ createS3bucket.py
@@ -6,8 +6,21 @@ bucket_name='big-thing-happen-big1'
 s3_location={
     'LocationConstraint': 'us-east-2'
 }
-try:
+
+def bucket_exist(bucket_name):
+    s3 = boto3.resource('s3')
+    return s3.bucket(bucket_name) in s3.bucket.all()
+
+if bucket_exist(bucket_name):
+    print('the bucket already exist')
+    #Do nothing
+
+else:
+    #create bucket
     s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-    print('Bucket created')
-except s3client.BucketAlreadyExists as err:
-    print('Error Mesaage: {}'.format(err.response['Error']['Message']))
+    print('the bucket created')
+
+
+#     s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+# except s3client.BucketAlreadyExists as err:
+#     print('Error Mesaage: {}'.format(err.response['Error']['Message']))
