48bec631fbdf3f1251b0bb0f7b9cef4ec0b9dda3
Rene-Ngong-N <106627399+Rene-Ngong-N@users.noreply.github.com>
2022-09-12 19:32:21 -0400

    Update createS3bucket.py

--- createS3bucket.py
+++ createS3bucket.py
@@ -2,24 +2,22 @@ import boto3
 
 sess= boto3.Session(region_name='us-east-2')
 s3client = sess.client('s3')
-bucket_name='big-thing-happen-big1'
+bucket_name='good-lets-see-3'
 s3_location={
     'LocationConstraint': 'us-east-2'
 }
 
-def bucket_exist(bucket_name):
-    s3 = boto3.resource('s3')
-    return s3.bucket(bucket_name) in s3.bucket.all()
+def bucket_exists(bucket_name):
+  s3 = boto3.resource('s3')
+  return s3.Bucket(bucket_name) in s3.buckets.all()
 
-if bucket_exist(bucket_name):
-    print('the bucket already exist')
-    #Do nothing
-
+if bucket_exists(bucket_name):
+  print('the bucket already exists!')
+  #Do nothing
 else:
-    #create bucket
-    s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-    print('the bucket created')
-
+  #create bucket
+  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+  print('the bucket created')
 
 #     s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
 # except s3client.BucketAlreadyExists as err:
