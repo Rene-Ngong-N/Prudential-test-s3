bf75633a76e4d819ee53ee0ee673f38e45d003a2
Rene-Ngong-N <106627399+Rene-Ngong-N@users.noreply.github.com>
2022-09-15 15:04:42 -0500

    Update createS3bucket.py

--- createS3bucket.py
+++ createS3bucket.py
@@ -1,54 +1,54 @@
-import sys
-import boto3
+#import sys
+#import boto3
 
-sess= boto3.Session(region_name='us-east-2')
-s3client = sess.client('s3')
-# command line argument for bucket name
-bucket_name = sys.argv[1]
+#sess= boto3.Session(region_name='us-east-2')
+#s3client = sess.client('s3')
+## command line argument for bucket name
+#bucket_name = sys.argv[1]
 
-s3_location={
-    'LocationConstraint': 'us-east-2'
-}
+#s3_location={
+#    'LocationConstraint': 'us-east-2'
+#}
 
-def bucket_exists(bucket_name):
-  s3 = boto3.resource('s3')
-  return s3.Bucket(bucket_name) in s3.buckets.all()
+#def bucket_exists(bucket_name):
+#  s3 = boto3.resource('s3')
+#  return s3.Bucket(bucket_name) in s3.buckets.all()
 
-if bucket_exists(bucket_name):
-  print('the bucket already exists!')
+#if bucket_exists(bucket_name):
+#  print('the bucket already exists!')
   #Do nothing
-else:
+#else:
   #create bucket
-  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-  print('the bucket created')
+#  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+#  print('the bucket created')
 
 
 
 
 
 
-# #!/bin/bash/env python3
-# import boto3
+ #!/bin/bash/env python3
+ import boto3
 
-# sess= boto3.Session(region_name='us-east-2')
-# s3client = sess.client('s3')
-# bucket_name='good-trying3'
-# s3_location={
-#     'LocationConstraint': 'us-east-2'
-# }
+ sess= boto3.Session(region_name='us-east-2')
+ s3client = sess.client('s3')
+ bucket_name='good-trying3'
+ s3_location={
+     'LocationConstraint': 'us-east-2'
+ }
 
-# def bucket_exists(bucket_name):
-#   s3 = boto3.resource('s3')
-#   return s3.Bucket(bucket_name) in s3.buckets.all()
+ def bucket_exists(bucket_name):
+   s3 = boto3.resource('s3')
+   return s3.Bucket(bucket_name) in s3.buckets.all()
 
-# if bucket_exists(bucket_name):
-#   print('the bucket already exists!')
-#   #Do nothing
-# else:
-#   #create bucket
-#   s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-#   print('the bucket created')
+ if bucket_exists(bucket_name):
+   print('the bucket already exists!')
+   #Do nothing
+ else:
+   #create bucket
+   s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+   print('the bucket created')
 
-#     s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-# except s3client.BucketAlreadyExists as err:
-#     print('Error Mesaage: {}'.format(err.response['Error']['Message']))
+     s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+ except s3client.BucketAlreadyExists as err:
+     print('Error Mesaage: {}'.format(err.response['Error']['Message']))
