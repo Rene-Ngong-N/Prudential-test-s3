c40855ef774fad9711d4d1a5d70206ee9bdb399f
Rene-Ngong-N <renengong16@gmail.com>
2022-09-15 16:54:13 -0400

    updating createS3bucket script for jenkins

--- createS3bucket.py
+++ createS3bucket.py
@@ -50,17 +50,17 @@ s3_location={
     'LocationConstraint': 'us-east-2'
 }
 
- def bucket_exists(bucket_name):
-   s3 = boto3.resource('s3')
-   return s3.Bucket(bucket_name) in s3.buckets.all()
+def bucket_exists(bucket_name):
+  s3 = boto3.resource('s3')
+  return s3.Bucket(bucket_name) in s3.buckets.all()
 
- if bucket_exists(bucket_name):
-   print('the bucket already exists!')
+if bucket_exists(bucket_name):
+  print('the bucket already exists!')
    #Do nothing
- else:
+else:
    #create bucket
-   s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-   print('the bucket created')
+  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+  print('the bucket created')
 
 
 
