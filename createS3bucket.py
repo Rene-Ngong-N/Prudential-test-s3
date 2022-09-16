4d813092a8a46e69d48b315937bae6e50aa10b85
Rene-Ngong <renengong16@gmail.com>
2022-09-16 14:14:03 -0500

    adding new file job

--- createS3bucket.py
+++ createS3bucket.py
@@ -1,4 +1,16 @@
 
+# import sys
+# import boto3
+
+# sess= boto3.Session(region_name='us-east-2')
+# s3client = sess.client('s3')
+# # command line argument for bucket name
+# bucket_name = sys.argv[1]
+
+# s3_location={
+#     'LocationConstraint': 'us-east-2'
+# }
+
 import sys
 import boto3
 
@@ -8,32 +20,20 @@ s3client = sess.client('s3')
 bucket_name = sys.argv[1]
 
 s3_location={
-    'LocationConstraint': 'us-east-2'
+   'LocationConstraint': 'us-east-2'
 }
 
-#import sys
-#import boto3
+def bucket_exists(bucket_name):
+ s3 = boto3.resource('s3')
+ return s3.Bucket(bucket_name) in s3.buckets.all()
 
-#sess= boto3.Session(region_name='us-east-2')
-#s3client = sess.client('s3')
-## command line argument for bucket name
-#bucket_name = sys.argv[1]
-
-#s3_location={
-#    'LocationConstraint': 'us-east-2'
-#}
-
-#def bucket_exists(bucket_name):
-#  s3 = boto3.resource('s3')
-#  return s3.Bucket(bucket_name) in s3.buckets.all()
-
-#if bucket_exists(bucket_name):
-#  print('the bucket already exists!')
-  #Do nothing
-#else:
-  #create bucket
-#  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-#  print('the bucket created')
+if bucket_exists(bucket_name):
+ print('the bucket already exists!')
+  # Do nothing
+else:
+  # create bucket
+ s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+ print('the bucket created')
 
 
 
