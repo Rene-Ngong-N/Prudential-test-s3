ac9b01fd1de5c9ee604be674a4b6931452ea93f0
Rene-Ngong <renengong16@gmail.com>
2022-09-20 17:16:49 -0500

    test with github action

--- createS3bucket.py
+++ createS3bucket.py
@@ -10,57 +10,57 @@
 # s3_location={
 #     'LocationConstraint': 'us-east-2'
 # }
-################# This section is for aws copepipeline
-import sys
-import boto3
-
-sess= boto3.Session(region_name='us-east-2')
-s3client = sess.client('s3')
-# command line argument for bucket name
-bucket_name = sys.argv[1]
-
-s3_location={
-   'LocationConstraint': 'us-east-2'
-}
-
-def bucket_exists(bucket_name):
- s3 = boto3.resource('s3')
- return s3.Bucket(bucket_name) in s3.buckets.all()
-
-if bucket_exists(bucket_name):
- print('the bucket already exists!')
-  # Do nothing
-else:
-  # create bucket
- s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
- print('the bucket created')
-
-
-#######################The above is for aws codepipeline
-
-
-
-#  #!/bin/bash/env python3
+# ################# This section is for aws copepipeline
+# import sys
 # import boto3
 
 # sess= boto3.Session(region_name='us-east-2')
 # s3client = sess.client('s3')
-# bucket_name='good-trying'
+# # command line argument for bucket name
+# bucket_name = sys.argv[1]
+
 # s3_location={
-#     'LocationConstraint': 'us-east-2'
+#    'LocationConstraint': 'us-east-2'
 # }
 
 # def bucket_exists(bucket_name):
-#   s3 = boto3.resource('s3')
-#   return s3.Bucket(bucket_name) in s3.buckets.all()
+#  s3 = boto3.resource('s3')
+#  return s3.Bucket(bucket_name) in s3.buckets.all()
 
 # if bucket_exists(bucket_name):
-#   print('the bucket already exists!')
-#    #Do nothing
+#  print('the bucket already exists!')
+#   # Do nothing
 # else:
-#    #create bucket
-#   s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
-#   print('the bucket created')
+#   # create bucket
+#  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+#  print('the bucket created')
+
+
+# #######################The above is for aws codepipeline
+
+
+
+ #!/bin/bash/env python3
+import boto3
+
+sess= boto3.Session(region_name='us-east-2')
+s3client = sess.client('s3')
+bucket_name='good-trying'
+s3_location={
+    'LocationConstraint': 'us-east-2'
+}
+
+def bucket_exists(bucket_name):
+  s3 = boto3.resource('s3')
+  return s3.Bucket(bucket_name) in s3.buckets.all()
+
+if bucket_exists(bucket_name):
+  print('the bucket already exists!')
+   #Do nothing
+else:
+   #create bucket
+  s3client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=s3_location)
+  print('the bucket created')
 
 
 
